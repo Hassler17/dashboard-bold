e8c927b1a173d54adf7a17a1b0a7a57949c1ecc4
Hassler17 <khferreira@misena.edu.co>
2024-07-06 17:50:45 -0500

    Add out folder

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 module.exports = {
     async rewrites() {
-        return [
-            {
-                source: '/prueba/bold/transacciones/:path*',
-                destination: 'https://bold-fe-api.vercel.app/api/:path*'
-            }
-        ]
-    }
-}
+      return [
+        {
+          source: '/prueba/bold/transacciones/:path*',
+          destination: 'https://bold-fe-api.vercel.app/api/:path*',
+        },
+      ];
+    },
+    trailingSlash: true,
+    basePath: '',
+    images: {
+      loader: 'imgix',
+      path: '',
+    },
+    distDir: 'out', // Esto especifica el directorio de salida para Next.js
+  };
